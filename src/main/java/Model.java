e8bd671681d17143a1a30acc441110f26cd87d64
Vincent Liu <liuvincent19915@gmail.com>
2017-03-02 09:21:18 -0500

    Got Gradle to work
    title says it

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,5 +1,13 @@
+import org.sdmlib.models.classes.ClassModel;
+
 /**
- * Created by Wave on 2/28/2017.
+ * Created by Wave on 3/2/2017.
  */
 public class Model {
+    public static void main(String[] arg) {
+        ClassModel model = new ClassModel("swe443.assignment5.mancala");
+
+        model.generate();
+    }
+
 }
